5974403e4ff89aa3987ae6b4b5ab41415380da8e
badger <energetic.honey.badger@gmail.com>
2026-05-01 11:20:42 -0400

    save

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import { HeaderText } from "~/Components/HeaderText/HeaderText";
-
+import { Searchbar } from "~/Components/Searchbar/Searchbar";
+import styles from './Home.module.css'
 export function meta() {
   return [
     { title: "New React Router App" },
@@ -8,5 +9,12 @@ export function meta() {
 }
 
 export default function Home() {
-  return <HeaderText header="New React App"/>;
+  return (
+    <div>
+      <Searchbar />
+      <div className={styles.main_content}>
+        <HeaderText header="New React App" />
+      </div>
+    </div>
+  )
 }
